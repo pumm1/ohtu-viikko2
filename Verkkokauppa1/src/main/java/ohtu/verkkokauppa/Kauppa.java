8caf3c2dc3e2e0d7239f4ef637bd30beb93825ea
pumm1 <patsu01@gmail.com>
2017-11-08 11:04:37 +0200

    verkkis alustus

---
+++ Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
@@ -0,0 +1,44 @@
+package ohtu.verkkokauppa;
+
+public class Kauppa {
+
+    private IO varasto;
+//    private Pankki pankki;
+    private Ostoskori ostoskori;
+    private IO viitegeneraattori;
+    private String kaupanTili;
+    private IO pankki;
+
+    public Kauppa( Varasto v, Pankki p, Viitegeneraattori vg) {
+        varasto = v;
+        pankki = p;
+        viitegeneraattori = vg;
+        kaupanTili = "33333-44455";
+    }
+
+    public void aloitaAsiointi() {
+        ostoskori = new Ostoskori();
+    }
+
+    public void poistaKorista(int id) {
+        Tuote t = varasto.haeTuote(id);
+        varasto.palautaVarastoon(t);
+        
+    }
+
+    public void lisaaKoriin(int id) {
+        if (varasto.saldo(id) > 0) {
+            Tuote t = varasto.haeTuote(id);
+            ostoskori.lisaa(t);
+            varasto.otaVarastosta(t);
+        }
+    }
+
+    public boolean tilimaksu(String nimi, String tiliNumero) {
+        int viite = viitegeneraattori.uusi();
+        int summa = ostoskori.hinta();
+
+        return pankki.tilisiirto(nimi, viite, tiliNumero, kaupanTili, summa);
+    }
+
+}
